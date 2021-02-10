1fe9288fe6d93cee931627bfd4bc3cc8aa9c82c7
botolo <datborx@gmail.com>
2021-02-10 09:35:47 +0100

    <09:35>

--- ProyectoIntegrado/src/app/app-routing.module.ts
+++ ProyectoIntegrado/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './componentes/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'index' },
+  { path: 'index', component: IndexComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ProyectoIntegrado/src/app/app.module.ts
+++ ProyectoIntegrado/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './componentes/index/index.component';
+import { ServicioComponent } from './componentes/servicio/servicio.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent, 
+    ServicioComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
