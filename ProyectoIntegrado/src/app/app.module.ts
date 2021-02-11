8189bf8ae1505951e4506647a3c13f201a941971
josemaria9 <josemaria070500@gmail.com>
2021-02-11 10:26:45 +0100

    Bootstrap Jm and Jaime

--- ProyectoIntegrado/src/app/app.module.ts
+++ ProyectoIntegrado/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './componentes/index/index.component';
-import { ServicioComponent } from './componentes/servicio/servicio.component';
 import { PerfilComponent } from './componentes/perfil/perfil.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
@@ -15,7 +14,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   declarations: [
     AppComponent,
     IndexComponent, 
-    ServicioComponent,
     PerfilComponent,
     RegistroComponent,
     InicioComponent,
