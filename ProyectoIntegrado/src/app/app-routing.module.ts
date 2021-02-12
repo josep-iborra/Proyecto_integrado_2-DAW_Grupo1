09d61ca4b415a2d64907f5023a62f0310023ccd2
josemaria9 <josemaria070500@gmail.com>
2021-02-12 08:41:07 +0100

    update login and register

--- ProyectoIntegrado/src/app/app-routing.module.ts
+++ ProyectoIntegrado/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './componentes/formularios/login/login.component';
+import { RegisterComponent } from './componentes/formularios/register/register.component';
 import { IndexComponent } from './componentes/index/index.component';
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'index' },
-  { path: 'index', component: IndexComponent }
+  { path: 'index', component: IndexComponent },
+  {path:'login' , component:LoginComponent},
+  {path:'register', component:RegisterComponent}
 ];
 
 @NgModule({
