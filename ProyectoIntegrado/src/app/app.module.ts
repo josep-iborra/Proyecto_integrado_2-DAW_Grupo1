b2b235596e9dadbb79a2f831f13b0a0a8e87998e
josep-iborra <joibpo@floridauniversitaria.es>
2021-02-12 10:35:02 +0100

    <10:34>

--- ProyectoIntegrado/src/app/app.module.ts
+++ ProyectoIntegrado/src/app/app.module.ts
@@ -7,20 +7,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './componentes/index/index.component';
 import { PerfilComponent } from './componentes/perfil/perfil.component';
-import { InicioComponent } from './componentes/inicio/inicio.component';
 import { AddServicioComponent } from './componentes/add-servicio/add-servicio.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProductosComponent } from './componentes/productos/productos.component';
 import { RegisterComponent } from './componentes/formularios/register/register.component';
 import { MensajesComponent } from './componentes/formularios/mensajes/mensajes.component';
-import { EditarPerfilComponent } from './formularios/editar-perfil/editar-perfil.component';
+import { EditarPerfilComponent } from './componentes/formularios/editar-perfil/editar-perfil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
     PerfilComponent,
-    InicioComponent,
     AddServicioComponent,
     ProductosComponent,
     RegisterComponent,
