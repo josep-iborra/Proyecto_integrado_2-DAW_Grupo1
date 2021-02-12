ff08b5313ca0fff2aea647194990f2d69d3c7dbd
josemaria9 <josemaria070500@gmail.com>
2021-02-12 08:31:40 +0100

    update

--- ProyectoIntegrado/src/app/app-routing.module.ts
+++ ProyectoIntegrado/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './componentes/index/index.component';
-import {Register}
+
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'index' },
