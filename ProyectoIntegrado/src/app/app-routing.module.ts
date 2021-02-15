9f5a0c62112e7259464c37b474481fc68021fcb1
josemaria9 <josemaria070500@gmail.com>
2021-02-15 12:21:16 +0100

    modified forms

--- ProyectoIntegrado/src/app/app-routing.module.ts
+++ ProyectoIntegrado/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AddServicioComponent } from './componentes/add-servicio/add-servicio.component';
 import { ContactanosComponent } from './componentes/formularios/contactanos/contactanos.component';
 import { ContactoComponent } from './componentes/formularios/contacto/contacto.component';
+import { EditarPerfilComponent } from './componentes/formularios/editar-perfil/editar-perfil.component';
 import { LoginComponent } from './componentes/formularios/login/login.component';
 import { MensajesComponent } from './componentes/formularios/mensajes/mensajes.component';
 import { RegisterComponent } from './componentes/formularios/register/register.component';
@@ -22,6 +23,7 @@ const routes: Routes = [
   { path: 'mensajes', component: MensajesComponent},
   { path: 'register', component: RegisterComponent},
   {path:'contactanos', component:ContactanosComponent},
+  {path:'editarPerfil', component:EditarPerfilComponent},
   { path: '**', component: IndexComponent},
   
 
