bad075f7c1fd03cd8da130c4ef1e45dc078fe0f4
josep-iborra <57388708+josep-iborra@users.noreply.github.com>
2021-02-16 12:05:49 +0100

    Updated_bdd

--- proyecto_bdd.sql
+++ proyecto_bdd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-02-2021 a las 20:12:22
+-- Tiempo de generación: 16-02-2021 a las 12:04:40
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 7.4.15
 
@@ -52,6 +52,26 @@ INSERT INTO `categorias` (`ID`, `Nombre`) VALUES
 (15, 'Para niños'),
 (16, 'Servicios bodas'),
 (17, 'Teatro');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `doctrine_migration_versions`
+--
+
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20210216074257', '2021-02-16 09:06:16', 520),
+('DoctrineMigrations\\Version20210216080723', '2021-02-16 09:07:30', 1407);
 
 -- --------------------------------------------------------
 
@@ -116,67 +136,76 @@ CREATE TABLE `subcategorias` (
 --
 
 INSERT INTO `subcategorias` (`ID`, `Nombre`, `ID_categoria`) VALUES
-(1, 'Organizadores de eventos', 0),
-(2, 'Representantes musicales', 0),
+(1, 'organizacion_de_eventos', 0),
+(2, 'representantes_musicales', 0),
 (3, 'Actores', 1),
 (4, 'Chefs', 1),
-(5, 'Deportistas', 1),
-(6, 'Influencers', 1),
-(7, 'Cantantes de beatbox', 2),
-(8, 'Cantantes de copla', 2),
-(9, 'Cantantes de jazz', 2),
-(10, 'Cantantes de rap', 2),
-(11, 'Cantantes de rock', 2),
-(12, 'Alquiler de carpas', 3),
-(13, 'Cortador de jamon', 3),
-(14, 'Food truck', 3),
-(15, 'Paellas gigantes', 3),
-(16, 'Tartas gigantes', 3),
-(17, 'Payasos', 4),
-(18, 'Mimos', 4),
+(7, 'Cantantes_de_beatbox', 2),
+(8, 'cantantes_de_copla', 2),
+(9, 'cantantes_de_jazz', 2),
+(10, 'cantantes_de_rap', 2),
+(11, 'cantantes_de_rock', 2),
+(12, 'alquiler_de_carpas', 3),
+(13, 'cortador_de_jamon', 3),
+(14, 'food_truck', 3),
+(15, 'paellas_gigantes', 3),
+(16, 'tartas_gigantes', 3),
+(17, 'payasos', 4),
+(18, 'mimos', 4),
 (19, 'malabaristas', 4),
-(20, 'Ventrilocuos', 4),
-(21, 'Zancudos', 4),
-(22, 'Comicos', 5),
-(23, 'Grupos de teatro', 5),
-(24, 'Monologistas', 5),
-(25, 'Imitadores', 5),
-(26, 'Bailarinas Danza del vientre', 6),
-(27, 'Bailarines de Tango', 6),
-(28, 'Bailarines de Flamenco', 6),
-(29, 'Compañias de danza', 6),
-(30, 'Danza contemporania', 6),
-(31, 'Disc Jockey', 7),
-(32, 'Disco movil', 7),
-(33, 'Caricaturas', 8),
-(34, 'Castillos hinchables', 8),
-(35, 'Maquina de espuma', 8),
-(36, 'Cine al aire libre', 8),
-(37, 'Baile Flamenco', 10),
-(38, 'Grupos de rumba', 10),
-(39, 'Grupos de flamenco', 10),
-(40, 'Grupos de Sevillanas', 10),
-(41, 'Escapistas', 12),
-(42, 'Hipnotozadores', 12),
-(43, 'Magos', 12),
-(44, 'Mentalista', 12),
-(45, 'Ilusionista', 12),
-(46, 'Clarinetistas', 13),
-(47, 'Flautistas', 13),
-(48, 'Pianistas', 13),
-(49, 'Trompetistas', 13),
-(50, 'Animadores', 15),
-(51, 'Futbolin humano', 15),
-(52, 'Pinta caritas', 15),
-(53, 'Globoflexia', 15),
-(54, 'Barman', 16),
-(55, 'Pirotecnias', 16),
-(56, 'Maestro de ceremonias', 16),
-(57, 'Coches de boda', 16),
-(58, 'Teatro de marionetas', 17),
-(59, 'Sombras chinescas', 17),
-(60, 'Grupos de treato', 17),
-(61, 'Actores de teatro', 17);
+(20, 'ventrilocuos', 4),
+(22, 'comicos', 5),
+(23, 'grupo_de_teatro', 5),
+(24, 'monologistas', 5),
+(25, 'imitadores', 5),
+(26, 'bailarinas_de_la_danza_del_vientre', 6),
+(27, 'bailarines_de_tango', 6),
+(29, 'compania_de_danza', 6),
+(30, 'danza_contemporania', 6),
+(31, 'disc_jockey', 7),
+(32, 'disco_movil', 7),
+(34, 'castillos_hinchables', 8),
+(35, 'maquina_de_espuma', 8),
+(36, 'cine_al_aire_libre', 8),
+(37, 'grupos_de_rumba', 10),
+(38, 'grupos_de_flamenco', 10),
+(39, 'grupos_de_sevillanas', 10),
+(41, 'hipnotizadores', 12),
+(42, 'magos', 12),
+(43, 'mentalistas', 12),
+(44, 'ilusionistas', 12),
+(45, 'escapistas', 12),
+(46, 'clarinetistas', 13),
+(47, 'flautistas', 13),
+(48, 'pianistas', 13),
+(49, 'trompetistas', 13),
+(50, 'animadores', 15),
+(51, 'futbolin_humano', 15),
+(52, 'pinta_caritas', 15),
+(53, 'globoflexia', 15),
+(54, 'barman', 16),
+(55, 'pirotecnia', 16),
+(56, 'maestro_de_ceremonias', 16),
+(57, 'coches_de_boda', 16),
+(59, 'sombras_chinescas', 17),
+(62, 'teatro_de_marionetas', 17);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` longtext COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '(DC2Type:json)',
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellidos` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `telefono` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id_empresa` varchar(11) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `api_token` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -195,6 +224,13 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`ID`, `Nombre`, `Email`, `password`, `apellidos`, `telefono`, `id_empresa`) VALUES
+(1, 'pepo', 'Pepo@gmail.com', '1234', 'pacp', '565432145', NULL);
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -203,6 +239,12 @@ CREATE TABLE `usuarios` (
 --
 ALTER TABLE `categorias`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `doctrine_migration_versions`
+--
+ALTER TABLE `doctrine_migration_versions`
+  ADD PRIMARY KEY (`version`);
 
 --
 -- Indices de la tabla `empresa`
@@ -227,6 +269,13 @@ ALTER TABLE `productos`
 --
 ALTER TABLE `subcategorias`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -266,13 +315,19 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `subcategorias`
 --
 ALTER TABLE `subcategorias`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
