a34fda06260aaed6ea097fb5ca9512e47f0e6560
botolo <datborx@gmail.com>
2021-02-16 09:20:39 +0100

    <09:20>

--- ProyectoIntegrado/src/app/componentes/formularios/login/login.component.ts
+++ ProyectoIntegrado/src/app/componentes/formularios/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsuarioService } from './../../../servicios/usuario.service';
+import { FormGroup, FormBuilder, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  userForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public formBuilder: FormBuilder,
+    private router: Router,
+    private usuariosService: UsuarioService,
+    private ngZone: NgZone
+  ) {
+    this.userForm = this.formBuilder.group({
+      email: [''],
+      password: ['']
+    })
   }
 
+  ngOnInit() { }
+
+  onSubmit(): any {
+    console.log(this.userForm.value);
+    this.usuariosService.AddUser(this.userForm.value)
+      .subscribe(() => {
+        console.log('Data added successfully!')
+        this.ngZone.run(() => this.router.navigateByUrl('/index'))
+      }, (err) => {
+        console.log(err);
+      });
+  }
 }
