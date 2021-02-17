8ab03ac0e00c2d64c833380fbf30ec35ffdcbbd6
botolo <datborx@gmail.com>
2021-02-17 09:50:50 +0100

    <09:50>

--- ProyectoIntegrado/src/app/app.component.ts
+++ ProyectoIntegrado/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { from } from 'rxjs';
-import { User } from 'src/app/servicios/usuario.service'
-import { UsuarioService } from 'src/app/servicios/usuario.service'
+import { User } from 'src/app/servicios/usuario.service';
+import { UsuarioService } from 'src/app/servicios/usuario.service';
 
 @Component({
   selector: 'app-root',
@@ -15,5 +15,15 @@ export class AppComponent {
   constructor(private usuarioService: UsuarioService) {
     this.user = this.usuarioService.userValue;
     console.log(this.user);
+  }
+
+  checkLogin = () => {
+    this.user = this.usuarioService.userValue;
+    if (this.user.id == undefined) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 }
