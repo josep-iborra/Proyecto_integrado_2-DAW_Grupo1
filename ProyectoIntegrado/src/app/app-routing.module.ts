9dd2c089d75adaa2e7643dd37ac80f6b0c778b2a
Panihe <email@example.com>
2021-02-17 10:37:58 +0100

    Actualizado app-routing

--- ProyectoIntegrado/src/app/app-routing.module.ts
+++ ProyectoIntegrado/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AddServicioComponent } from './componentes/add-servicio/add-servicio.co
 import { ContactanosComponent } from './componentes/formularios/contactanos/contactanos.component';
 import { ContactoComponent } from './componentes/formularios/contacto/contacto.component';
 import { EditarPerfilComponent } from './componentes/formularios/editar-perfil/editar-perfil.component';
+import { EnvioMensajeComponent } from './componentes/formularios/envio-mensaje/envio-mensaje.component';
 import { LoginComponent } from './componentes/formularios/login/login.component';
 import { MensajesComponent } from './componentes/formularios/mensajes/mensajes.component';
 import { RegisterComponent } from './componentes/formularios/register/register.component';
@@ -24,7 +25,8 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent},
   { path: 'contactanos', component: ContactanosComponent},
   { path: 'vendedor', component: VendedorComponent},
-  {path:'editarPerfil', component: EditarPerfilComponent},
+  { path: 'editarPerfil', component: EditarPerfilComponent},
+  { path: 'mensaje-nuevo', component: EnvioMensajeComponent},
   { path: '**', component: IndexComponent},
 
   
