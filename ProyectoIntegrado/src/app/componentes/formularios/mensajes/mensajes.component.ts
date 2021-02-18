7bcf168ca29a0ebc88f616ee4e4362f7130c2497
Panihe <email@example.com>
2021-02-18 11:23:07 +0100

    Actualizado mensajes

--- ProyectoIntegrado/src/app/componentes/formularios/mensajes/mensajes.component.ts
+++ ProyectoIntegrado/src/app/componentes/formularios/mensajes/mensajes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-mensajes',
   templateUrl: './mensajes.component.html',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MensajesComponent implements OnInit {
 
-  constructor() { }
+  closeResult = '';
+  content: any;
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
 
+  open(content: any) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
+
+
